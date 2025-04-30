fa16c02cfbb2a5ed913ff7e0eb097bfd7bc83d72
astratow <davno_pora@mail.com>
2025-04-30 21:29:17 +0100

    responsive

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -29,38 +29,53 @@ export default function UserTable({ users, onEdit, onDelete} : UserTableProps) {
             <table className="min-w-full bg-white rounded-lg shadow-sm overflow-hidden">
                 <thead>
                     <tr className="bg-gray-100 text-gray-700 text-sm uppercase">
-                        <th className="text-left px-6 py-3">Name</th>
-                        <th className="text-left px-6 py-3">Email</th>
-                        <th className="text-left px-6 py-3">Status</th>
-                        <th className="text-left px-6 py-3">Action</th>
+                        <th className="text-left p-2 sm:px-6 sm:py-3">Name</th>
+                        <th className="text-left p-2 sm:px-6 sm:py-3 hidden md:table-cell">Email</th>
+                        <th className="text-left p-2 sm:px-6 sm:py-3 hidden md:table-cell">Status</th>
+                        <th className="text-left p-2 sm:px-6 sm:py-3">Action</th>
                     </tr>
                 </thead>
                 <tbody className="text-gray-700">
                     {users.map((user, idx) => (
-                        <tr key={user.UserID} className={idx % 2 === 0 ? 'bg-gray-50' : 'bg-white'}>
-                            <td className="px-6 py-4 whitespace-nowrap">{user.DisplayName}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{user.Email}</td>
-                            <td className="px-6 py-4 whitespace-nowrap">{user.Status}</td>
-                            <td className="flex items-end px-6 py-4 whitespace-nowrap text-center space-x-2">
-                                <button 
-                                    className="text-indigo-600 hover:text-indigo-800"
-                                    onClick={() => onEdit(user)}
-                                    title="Edit User"
-                                    >
-                                        <PencilIcon className="h-5 w-5 contain-inline" />
-                                </button>
-                                <button
-                                    className="text-red-600 hover:text-red-800"
-                                    onClick={() => onDelete(user)}
-                                    title="Delete User"
-                                >
-                                    <TrashIcon className="h-5 w-5 inline" />
-                                </button>
-                            </td>
-                            
+                        <tr
+                        key={user.UserID}
+                        className={`${
+                            idx % 2 === 0 ? 'bg-gray-50' : 'bg-white'
+                        } border-b border-gray-200`}
+                        >
+
+                        <td className="p-2 sm:px-6 sm:py-4 whitespace-nowrap text-sm sm:text-base">
+                            <div className="font-medium text-gray-900">{user.DisplayName}</div>
+                            <div className="text-gray-500 text-sm md:hidden">{user.Email}</div>
+                        </td>
+
+                        <td className="sm:px-6 sm:py-4 whitespace-nowrap hidden md:table-cell">
+                            {user.Email}
+                        </td>
+
+                        <td className="p-2 sm:px-6 sm:py-4 whitespace-nowrap hidden md:table-cell">
+                            {user.Status}
+                        </td>
+
+                        <td className="flex items-end p-2 sm:px-6 sm:py-4 whitespace-nowrap text-center space-x-2">
+                            <button
+                            className="text-indigo-600 hover:text-indigo-800"
+                            onClick={() => onEdit(user)}
+                            title="Edit User"
+                            >
+                            <PencilIcon className="h-5 w-5 contain-inline" />
+                            </button>
+                            <button
+                            className="text-red-600 hover:text-red-800"
+                            onClick={() => onDelete(user)}
+                            title="Delete User"
+                            >
+                            <TrashIcon className="h-5 w-5 inline" />
+                            </button>
+                        </td>
                         </tr>
                     ))}
-                </tbody>
+                    </tbody>
             </table>
         </div>
     )
